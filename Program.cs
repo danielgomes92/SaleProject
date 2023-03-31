f4d0f00822a3c73e4c2b6c63e513f776d5c876cd
danielgomes92 <daniel.ferreirasg@hotmail.com>
2023-03-31 10:52:09 -0300

    add the service to call the metod who made the Seed in database

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<SaleProjectContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SaleProjectContext") ?? throw new InvalidOperationException("Connection string 'SaleProjectContext' not found.")));
 
+builder.Services.AddScoped<SeedingService>();
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -19,6 +21,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().SeedPopulateDataBase();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
