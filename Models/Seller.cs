b9a95f056d0244b6c0d01238f14bf89808821a39
danielgomes92 <daniel.ferreirasg@hotmail.com>
2023-03-31 10:51:46 -0300

    organization - fix

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -10,10 +10,8 @@
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
-        public Seller()
-        {
+        public Seller() { }
 
-        }
         public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
