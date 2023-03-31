e4c34cf3fa924ce2daa8950df7ed3b49f45f9ee5
danielgomes92 <daniel.ferreirasg@hotmail.com>
2023-03-31 18:19:34 -0300

    list to return department order by name

---
+++ Services/DepartmentService.cs
@@ -0,0 +1,14 @@
+﻿using SaleProject.Data;
+
+namespace SaleProject.Services
+{
+	public class DepartmentService
+	{
+		private SaleProjectContext _context;
+
+		public DepartmentService(SaleProjectContext context)
+		{
+			_context = context;
+		}
+	}
+}
