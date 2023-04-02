544590fd94b4e187a80e5f9745b443a9e2bc56e1
danielgomes92 <daniel.ferreirasg@hotmail.com>
2023-04-02 11:52:23 -0300

    change HTTP requests error to the redirection error page.

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -4,115 +4,122 @@ using SaleProject.Models;
 using SaleProject.Models.ViewModels;
 using SaleProject.Services;
 using SaleProject.Services.Exceptions;
+using System.Diagnostics;
 
 namespace SaleProject.Controllers
 {
-	public class SellersController : Controller
-	{
-		private readonly SellerService _sellerService;
-		private readonly DepartmentService _departmentService;
-		public SellersController(SellerService sellerService, DepartmentService departmentService)
-		{
-			_sellerService = sellerService;
-			_departmentService = departmentService;
-		}
+    public class SellersController : Controller
+    {
+        private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
+        {
+            _sellerService = sellerService;
+            _departmentService = departmentService;
+        }
 
-		public IActionResult Index()
-		{
-			var list = _sellerService.GetSellerList();
-			return View(list);
-		}
-		public IActionResult Create()
-		{
-			var departments = _departmentService.FindAll();
-			var viewModel = new SellerFormViewModel { Departments = departments };
-			return View(viewModel);
-		}
+        public IActionResult Index()
+        {
+            var list = _sellerService.GetSellerList();
+            return View(list);
+        }
+        public IActionResult Create()
+        {
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
+        }
 
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public IActionResult Create(Seller seller)
-		{
-			_sellerService.Insert(seller);
-			return RedirectToAction(nameof(Index));
-		}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
 
-		public IActionResult Delete(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound("This ID doesnt exist in database");
-			}
-			var obj = _sellerService.FindById(id.Value);
-			if (obj == null)
-			{
-				return NotFound(404);
-			}
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id not provided in database" });
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id doesnt exist in database" });
+            }
 
-			return View(obj);
-		}
+            return View(obj);
+        }
 
-		[HttpPost]
-		public IActionResult Delete(int id)
-		{
-			_sellerService.Remove(id);
-			return RedirectToAction(nameof(Index));
-		}
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
 
-		public IActionResult Details(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound("This ID doesnt exist in database");
-			}
-			var obj = _sellerService.FindById(id.Value);
-			if (obj == null)
-			{
-				return NotFound(404);
-			}
-			return View(obj);
-		}
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id not provided in database" });
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id doesnt exist in database" });
+            }
+            return View(obj);
+        }
 
-		public IActionResult Edit(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound("this id doesnt exist in database");
-			}
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id not provided in database" });
+            }
 
-			var obj = _sellerService.FindById(id.Value);
-			if (obj == null)
-			{
-				return NotFound("this id doesnt exist in database");
-			}
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id doesnt exist in database" });
+            }
 
-			List<Department> departments = _departmentService.FindAll();
-			SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
 
-			return View(viewModel);
-		}
+            return View(viewModel);
+        }
 
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public IActionResult Edit(int id, Seller seller)
-		{
-			if (id != seller.Id)
-			{
-				return BadRequest("what the fuc is that");
-			}
-			try
-			{
-				_sellerService.Update(seller);
-				return RedirectToAction(nameof(Index));
-			}
-			catch (NotFoundException)
-			{
-				return NotFound();
-			}
-			catch (DbCurrencyException)
-			{
-				return BadRequest();
-			}
-		}
-	}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id missmatch in database" });
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (ApplicationException error)
+            {
+                return RedirectToAction(nameof(Error), new { message = error.Message });
+            }
+        }
+
+        public IActionResult Error(string message)
+        {
+            var viewModel = new ErrorViewModel
+            {
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return View(viewModel);
+        }
+    }
 }
