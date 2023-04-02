a71b626f87f1f996be93ac2aff2ae5b6472cb034
danielgomes92 <daniel.ferreirasg@hotmail.com>
2023-04-02 11:25:18 -0300

    exceptions to error treatment

---
+++ Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace SaleProject.Services.Exceptions
+{
+	public class NotFoundException : ApplicationException
+	{
+		public NotFoundException(string message) : base(message)
+		{
+		}
+	}
+}
