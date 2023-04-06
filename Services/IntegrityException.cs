5d92f8dd2f7b34cfba52b7889e3922adff0000bb
danielgomes92 <daniel.ferreirasg@hotmail.com>
2023-04-06 11:20:37 -0300

    exceptions integrity added to fix error

---
+++ Services/IntegrityException.cs
@@ -0,0 +1,10 @@
+﻿namespace SaleProject.Services
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+
+        }
+    }
+}
